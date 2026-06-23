8c038c164b721459daf3c2674cd8fcc1adbea17a
kyrylohavryliuk <gavrilukkirilo@gmail.com>
2026-06-23 15:49:58 +0200

    adding ci/cd for user-service

---
+++ notification-service/src/test/java/com/wex/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.wex.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+}
